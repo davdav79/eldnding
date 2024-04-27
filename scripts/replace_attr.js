0991beac84dfbeea0a405a7bca7f60dc3b759817
davdav79 <d.eichhorn1@gmx.net>
2024-04-27 15:49:44 +0200

    adding hook back in

--- scripts/replace_attr.js
+++ scripts/replace_attr.js
@@ -1,3 +1,4 @@
+Hooks.on("ready", function() {
 game.dnd5e.config.abilities.cha.label = "Arcane"
 game.dnd5e.config.abilities.hon.label = "Mind"
 game.dnd5e.config.abilities.san.label = "Endurance"
@@ -8,3 +9,4 @@ game.dnd5e.config.abilities.hon.abbreviation = "min"
 game.dnd5e.config.abilities.san.abbreviation = "end"
 game.dnd5e.config.abilities.wis.abbreviation = "fai"
 game.dnd5e.config.abilities.con.abbreviation = "vig"
+});
