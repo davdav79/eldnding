fb63a36fadf08aa39de6f1fc019a1380b2f22c79
davdav79 <d.eichhorn1@gmx.net>
2024-04-27 15:23:39 +0200

    first try

---
+++ scripts/replace_attr.js
@@ -0,0 +1,11 @@
+
+  Hooks.on("ready", function() {
+    game.dnd5e.config.abilities.cha.label = "Arcane"
+    game.dnd5e.config.abilities.hon.label = "Mind"
+    game.dnd5e.config.abilities.san.label = "Endurance"
+    game.dnd5e.config.abilities.wis.label = "Faith"
+    game.dnd5e.config.abilities.cha.abbreviation = "arc"
+    game.dnd5e.config.abilities.hon.abbreviation = "min"
+    game.dnd5e.config.abilities.san.abbreviation = "end"
+    game.dnd5e.config.abilities.wis.abbreviation = "fai"
+  });
